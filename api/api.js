416692a5e5c79448dab13984501e7c7b4a056410
michael <michgboxy@gmail.com>
2017-09-30 04:03:57 -0700

    mounts the authentication router

--- api/api.js
+++ api/api.js
@@ -2,11 +2,14 @@ var express 		= require("express"),
 	api				= express.Router(),
 	artisteRouter 	= require("./v1/user/artiste-router.js");
 	adminRouter		= require("./v1/admin/admin-router.js");
+	auth			= require("./v1/auth/authRouter.js");
 	
 	//mount the routers on the API
 
+	api.use("/auth", auth);
 	api.use("/artiste", artisteRouter);
 	api.use("/admin", adminRouter);
+
 
 	module.exports = api;
 
