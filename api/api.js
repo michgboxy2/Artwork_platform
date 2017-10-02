cdaef9fe32e03990185fc062a5d8c2700a27c348
michael <michgboxy@gmail.com>
2017-10-02 10:06:38 -0700

    mounts the art router on the api

--- api/api.js
+++ api/api.js
@@ -3,12 +3,14 @@ var express 		= require("express"),
 	artisteRouter 	= require("./v1/user/artiste-router.js");
 	adminRouter		= require("./v1/admin/admin-router.js");
 	auth			= require("./v1/auth/authRouter.js");
+	artRouter 		= require("./v1/art/art-router.js");
 	
 	//mount the routers on the API
 
 	api.use("/auth", auth);
 	api.use("/artiste", artisteRouter);
 	api.use("/admin", adminRouter);
+	api.use("/art", artRouter);
 
 
 	module.exports = api;
